ca8a0a88947fae2969b44934171b984e57d6506a
JCBackendDev <jcbackenddev@gmail.com>
2021-12-09 11:43:57 +0100

    added timer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { TimerComponent } from './timer/timer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    TimerComponent
   ],
   imports: [
     BrowserModule,
