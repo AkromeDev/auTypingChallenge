40c9e2edd71f549eb384ee230976eb1a607dc5cd
JCBackendDev <jcbackenddev@gmail.com>
2021-12-10 09:18:41 +0100

    Commit second
    Little bug with my commit there!

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,7 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  randomText = 'blablaRandomText';
+  happyTrees = require('bob-ross-lipsum');
+  randomText = this.happyTrees();
   result = 'blablaResult';
 
   constructor() { }
