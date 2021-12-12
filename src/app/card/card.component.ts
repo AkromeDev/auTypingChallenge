e99216f630ea7030c62d21a5d274cca070708e2d
JCBackendDev <jcbackenddev@gmail.com>
2021-12-12 18:52:40 +0100

    first Analysing test letter by letter
    This is not easy! but I will find the solution! [innerHtml] seems to do the trick. I still need to do a bit more though!

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -11,43 +11,44 @@ export class CardComponent implements OnInit {
   randomText = '';
   userText = '';
   successMatch = false;
+  successMatchInTime = false;
+  renderedText= '';
   
   constructor() { }
   
   @ViewChild(TimerComponent) child!: TimerComponent;
-  
+  // timeNeeded = this.child.counter;
+
   startingTimer() {
     if (this.child.running === false) {
       this.child.startTimer();
     }
   }
-  
   onInput(input: string) {
-    this.userText = input;
+    this.renderedText = input;
+    this.compareSentences();
     if (this.userText === this.randomText) {
       this.successMatch = true;
       this.child.startTimer();
     }
   }
-
-  compareSentences(userSentence: string) {
-    for (let i = 0; i < userSentence.length; i++) {
-      if (userSentence.substring(i) === this.randomText.substring(i)) {
-        
+  compareSentences() {
+    for (let i = 0; i < this.randomText.length; i++) {
+      if (this.userText.substring(i) === null) {
+        this.renderedText += "<span style='background-color:powderblue;'>" + this.randomText.substring(i) + "<span>"
+      } else if (this.userText.substring(i) === this.randomText.substring(i)) {
+        this.renderedText += "<span style='background-color:powderblue;'>" + this.randomText.substring(i) + "<span>"
+      } else {
+        this.renderedText += "<span style='background-color:powderblue;'>" + this.randomText.substring(i) + "<span>"
       }
     }
   }
-
   newChallenge() {
-    this.randomText = this.happyTrees();
+    this.renderedText = this.happyTrees();
     this.child.clearTimer();
     this.successMatch = false;
   }
-  
   ngOnInit(): void {
-    this.randomText = this.happyTrees();
+    this.renderedText = this.happyTrees();
   }
-
-
-  
 }
